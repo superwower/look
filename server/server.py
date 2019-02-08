5eb1185899eacd1ec7dfd1b916c571bb65df601e
Otsuki Hitoshi <hitochan777@gmail.com>
2019-02-08 17:39:26 +0900

    WIP: implement attendance taking

---
+++ server/server.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+import urllib
+
+from .auth import FaceAuthenticator, UserRepository
+
+app = Flask(__name__)
+# user_repo = UserRepository()
+# face_auth = FaceAuthenticator(user_repo)
+
+@app.route('/api/auth', methods=['POST'])
+def authenticate():
+    # url_encoded = request.args.get('company-data')
+    # url_decoded = urllib.unquote(url_encoded).decode('utf8')
+    # company_data = json.loads(url_decoded)
+    # print(company_data)
+    # username, ok = face_auth.auth(target_filename)
+    
+    return {"username": username, "error": "" if ok else "Failed to authenticate"}
---
+++ server/webotr.py
@@ -0,0 +1,44 @@
+import requests
+from requests_ntlm import HttpNtlmAuth
+from enum import Enum
+import toml
+from typing import NamedTuple
+
+class UserAuthInfo(NamedTuple):
+    username: str
+    password: str 
+
+class AttendanceMode(Enum):
+    START = "01"
+    FINISH = "02"
+
+class AttendanceService:
+    def __init__(self, endpoint: str) -> None:
+        self.endpoint = endpoint
+
+    def ping(self, user: UserAuthInfo) -> str:
+        r = requests.get(self.endpoint, auth=HttpNtlmAuth(user.username,user.password), verify=False)
+        if r.status_code == requests.codes.ok:
+            return "Pong"
+
+        r.raise_for_status()
+
+    def submit(self, user: UserAuthInfo, mode: AttendanceMode = AttendanceMode.START) -> bool:
+        payload = {
+            "SubmitMode": mode
+        }
+        r = requests.post(self.endpoint, data=payload, auth=HttpNtlmAuth(user.username,user.password), verify=False)
+        if r.status_code == requests.codes.ok:
+            print(r.text)
+            return True
+
+        r.raise_for_status()
+
+
+if __name__ == "__main__":
+    with open("./config.toml", "r") as f:
+        config = toml.load(f)
+
+    user = UserAuthInfo(config["user"]["username"], config["user"]["password"])
+    service = AttendanceService(endpoint=config["endpoint"])
+    print(service.ping(user))
